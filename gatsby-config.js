e21fce9eccbedd8a9bacf6183466f1d262a30663
Luciano Júnior <luciannojunior@gmail.com>
2019-11-28 12:11:00 -0300

    Adding some opengraph data

--- gatsby-config.js
+++ gatsby-config.js
@@ -1,9 +1,16 @@
 module.exports = {
+  siteMetadata: {
+    title: `Início`,
+    titleTemplate: '%s | Loja Maçônica "Acácia da Borborema" nº 41',
+    siteUrl: `https://www.acaciadaborborema.com.br`,
+    description: `Loja Maçônica "Acácia da Borborema" da cidade de Campina Grande - PB. Filiada à Grande Loja Maçônica do Estado da Paraíba`,
+    image: '/images/acacia.jpg',
+  },
   plugins: [
     {
-      resolve: "@luciannojunior/gatsby-theme-lodge",
+      resolve: '@luciannojunior/gatsby-theme-lodge',
       options: {
-        basePath: "/",
+        basePath: '/',
       },
     },
   ],
