b5398219e8b19c4499d337b28d9627ec0725cd90
Luciano Júnior <luciannojunior@gmail.com>
2019-12-02 03:40:32 -0300

    Complementing 'Sobre a Loja' section and adding a couple of images. I'm still not satisfied with the markdown configuration, I think I will migrate to use something like Prismic in the near future.

--- src/components/Loja/index.js
+++ src/components/Loja/index.js
@@ -1,12 +1,13 @@
 import React from 'react'
 import classnames from 'classnames'
+import MD from 'react-markdown/with-html'
 import styles from './Loja.module.css'
 
-const Loja = () => (
-  <div className={'flex justify-center mv4'}>
-    <div className={styles.container}>
-      <h1 className="f3 f2-ns tl">Loja</h1>
-      <p className="tl">Em construção</p>
+const Loja = ({ texto = TEXTO }) => (
+  <div className={'flex justify-center mb4 tl'}>
+    <div className={classnames('mh4 tj', styles.container)}>
+      <h1 className="f3 f2-ns tl">Sobre a Loja</h1>
+      <MD source={texto} escapeHtml={false} />
     </div>
   </div>
 )
