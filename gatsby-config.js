06650f09dff179a1148c13f5ad8e7205d2aa7a9d
Luciano Júnior <luciannojunior@gmail.com>
2019-12-03 16:11:11 -0300

    Trying to get OG whatsapp image to work - again

--- gatsby-config.js
+++ gatsby-config.js
@@ -1,9 +1,9 @@
 module.exports = {
   siteMetadata: {
     title: `Início`,
-    titleTemplate: `Loja Maçônica "Acácia da Borborema" nº 41 | %s`,
+    titleTemplate: `Loja Maçônica Acácia da Borborema | %s`,
     url: `https://www.acaciadaborborema.com.br`,
-    description: `Loja Maçônica da cidade de Campina Grande - PB. Filiada à Grande Loja Maçônica do Estado da Paraíba`,
+    description: `Oriente de Campina Grande - PB. Filiada à Grande Loja Maçônica do Estado da Paraíba`,
     image: '/images/acacia_fb.png',
   },
   plugins: [
